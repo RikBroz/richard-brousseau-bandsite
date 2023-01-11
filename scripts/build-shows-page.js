f84082ce31d9f11047f08910c347030162822e83
RikBroz <rbrousseau896@gmail.com>
2023-01-11 00:12:58 -0600

    used axios to create HTTP request to get the array of show dates

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,11 +1,22 @@
-let showListings = [
-    { date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Francisco, CA" },
-    { date: "Tue Sept 21 2021", venue: "Pier 3 East", location: "San Francisco, CA" },
-    { date: "Mon Oct 15 2021", venue: "View Lounge", location: "San Francisco, CA" },
-    { date: "Mon Nov 06 2021", venue: "Hyatt Agency", location: "San Francisco, CA" },
-    { date: "Mon Nov 26 2021", venue: "Moscow Center", location: "San Francisco, CA" },
-    { date: "Mon Dec 15 2021", venue: "Press Club", location: "San Francisco, CA" },
-];
+axios
+    .get(`https://project-1-api.herokuapp.com/showdates?api_key=${api_key}`)
+    .then((response) => {
+        console.log(response.data);
+        renderShows(response.data);
+    }
+    ).catch((error) => {
+        console.log(error);
+    }
+);
+
+// let showListings = [
+//     { date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Francisco, CA" },
+//     { date: "Tue Sept 21 2021", venue: "Pier 3 East", location: "San Francisco, CA" },
+//     { date: "Mon Oct 15 2021", venue: "View Lounge", location: "San Francisco, CA" },
+//     { date: "Mon Nov 06 2021", venue: "Hyatt Agency", location: "San Francisco, CA" },
+//     { date: "Mon Nov 26 2021", venue: "Moscow Center", location: "San Francisco, CA" },
+//     { date: "Mon Dec 15 2021", venue: "Press Club", location: "San Francisco, CA" },
+// ];
 
 function createShowCard(show) {
     const divEl = document.createElement("div");
@@ -20,7 +31,38 @@ function createShowCard(show) {
     console.log(dateLabelEl.innerText);
 
     let dateContentEl = document.createElement("p");
-    dateContentEl.innerText = show.date;
+
+    const showDate = new Date(show.date);
+    console.log("show date is: " + showDate);
+    console.log("show date month was : " + (showDate.getMonth()+1));
+    console.log("show date UTC month was : " + (showDate.getUTCMonth()+1));
+    console.log("show date day was : " + showDate.getDate());
+    console.log("show date UTC day was : " + showDate.getUTCDate());
+    let showDate2 = `${showDate.getUTCDay()} ${((showDate.getUTCMonth()+1) < 10 ? '0' : '') + (showDate.getUTCMonth()+1)} ${(showDate.getUTCDate() < 10 ? '0' : '') + showDate.getUTCDate()} ${showDate.getUTCFullYear()}`;
+    console.log("updated showDate2: " + showDate2);
+    let showDateFormatted = new Date(showDate.getFullYear(), showDate.getMonth(), showDate.getUTCDate());
+    console.log("CORRECT formatted show date is: " + (showDateFormatted.toDateString()));
+
+    // const showDate2 = new Date(Date.UTC(showDate.getFullYear(), showDate.getMonth(), showDate.getDay()));
+    /* const showDate2 = new Date(Date.UTC(showDate.getFullYear(), showDate.getUTCMonth(), showDate.getUTCDate()));
+    console.log("CORRECT show date is: " + showDate2.toUTCString()); */
+    // console.log("CORRECT show date is: " + (showDate.getDate()+1));
+    //date.toLocaleString('en-US', { month: 'short' });
+
+    /* const showDateDay = (showDate.getDay()+1).toLocaleString('en-US', { weekday: 'long' });
+    console.log("show date day is: " + showDateDay);
+    console.log("formatted show date day is: " + showDate.toLocaleString('en-US', { weekday: 'short' }));
+    console.log("formatted show date month is: " + showDate.toLocaleString('en-US', { month: 'short' })); */
+    // let showDateFormatted = `${showDate.getUTCDay()} ${((showDate.getUTCMonth()+1) < 10 ? '0' : '') + (showDate.getUTCMonth()+1)} ${(showDate.getUTCDate() < 10 ? '0' : '') + showDate.getUTCDate()} ${showDate.getUTCFullYear()}`;
+   
+    // let showDateFormatted = `${showDate2.getUTCDay()} ${((showDate2.getUTCMonth()+1) < 10 ? '0' : '') + (showDate2.getUTCMonth()+1)} ${(showDate2.getUTCDate() < 10 ? '0' : '') + showDate2.getUTCDate()} ${showDate2.getUTCFullYear()}`;
+    /* let showDateFormatted = showDate2;
+    console.log("formatted show date is: " + showDateFormatted); */
+    // console.log("CORRECT formatted show date is: " + (showDate2.toDateString()));
+    /* console.log("CORRECT formatted show date is: " + (showDateFormatted.toDateString()));*/
+
+    // dateContentEl.innerText = show.date;
+    dateContentEl.innerText = showDateFormatted.toDateString();
     dateContentEl.classList.add("shows__data", "shows__data--date");
     console.log(dateContentEl.innerText);
 
@@ -30,7 +72,7 @@ function createShowCard(show) {
     console.log(venueLabelEl.innerText);
     
     let venueContentEl = document.createElement("p");
-    venueContentEl.innerText = show.venue;
+    venueContentEl.innerText = show.place;
     venueContentEl.classList.add("shows__data", "shows__data--venue");
     console.log(venueContentEl.innerText);
 
@@ -71,7 +113,8 @@ function createShowCard(show) {
     return divEl;
 }
 
-function renderShows() {
+// function renderShows() {
+function renderShows(showListings) {
     let showsEl = document.querySelector(".shows__section");
     console.log(showsEl);
     showsEl.innerHTML = "";
@@ -100,7 +143,7 @@ function renderShows() {
     }
 }
 
-renderShows();
+// renderShows();
 
 let sC = document.getElementsByClassName("shows__content");
 for (let i = 0; i < sC.length; i++) {
